cf16e31f7a2f88b60a6840ffcf255c9dbd309cc6
Naoki Ueda <naoki.dx.xb@gmail.com>
2015-07-13 02:45:02 +0900

    すこしだけ汎用的になるように修正

--- rest-service.py
+++ rest-service.py
@@ -1,17 +1,48 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+import tornado.escape
+from tornado import gen
+import tornado.httpserver
 import tornado.ioloop
+import tornado.options
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+from tornado.options import define, options
+
+define('port', default=8080, help="port to listen on")
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler),
+            (r"/diagnose", DiagnoseHandler),
+        ]
+        settings = dict(
+            debug=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def get_current_user(self):
+        return None
+
+class HomeHandler(BaseHandler):
     def get(self):
+        self.write({"status": 200, "message": "It works!!"})
+
+class DiagnoseHandler(BaseHandler):
+    def get(self):
+        # TODO
         self.write({"status": 200, "condition": 1})
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
 
 if __name__ == "__main__":
-    application.listen(8080)
-    tornado.ioloop.IOLoop.instance().start()
-
+    main()
