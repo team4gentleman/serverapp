d2eee07076e100d0efd7e31544b2cd953d8c0c93
Naoki Ueda <naoki.dx.xb@gmail.com>
2015-07-15 00:45:09 +0900

    微調整

--- 2_run_serverapp-test.sh
+++ 2_run_serverapp-test.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+#
+# docker run serverapp-test
+#
 docker run --name serverapp-test-up -i -t -d -p 8080:8080 \
   -v $GIT_HOME/serverapp:/opt/t4j/serverapp \
-  -v $GIT_HOME/chainerapp:/opt/t4j/chainerapp \
-  -v $GIT_HOME/chainer-data:/var/opt/t4j/chainer \
   t4j/serverapp-test:latest
