d26b68ae3f4387af507326b4fa5805f4f5498e61
Takehiro Yamazaki <nagafue@Takehiro-no-MacBook-Pro.local>
2015-07-15 07:48:26 +0900

    Pillow インストール追加、w-tornado コマンド追加

--- mobilenurserest.py
+++ mobilenurserest.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tornado
 import tornado.ioloop
 import tornado.web
@@ -7,11 +8,11 @@ from PIL import Image, ImageFilter
 __UPLOADS__ = "uploads/"
 
 # フォームから画像をアップするサンプル
- 
+
 class Userform(tornado.web.RequestHandler):
     def get(self):
         self.render("fileuploadform.html")
- 
+
 class Upload(tornado.web.RequestHandler):
     def post(self):
         fileinfo = self.request.files['filearg'][0]
@@ -23,7 +24,7 @@ class Upload(tornado.web.RequestHandler):
         print (fileinfo['body'])
         fh.write(fileinfo['body'])
         self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
- 
+
 
 # RESTできた画像を受け取って、chainappに流して、結果を流すサンプル（作成中）
 
@@ -40,17 +41,17 @@ class MobileNurseRest(tornado.web.RequestHandler):
         cname = str(uuid.uuid4()) + ".jpg"
         fh = open(__UPLOADS__ + cname, 'wb')
         fh.write(self.request.body)
-        
+
         # chainerappを呼んで結果をもらう★
         # ・・・
         # ・・・
-        
+
         # 結果に従い、ファイルを加工し、それをバイナリで返す。★
         # cname_2 = str(uuid.uuid4()) + ".jpg"
         # fh_2 = open(__UPLOADS__ + cname, 'wb')
         # fh_2.write(self.request.body)
         # self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
-        
+
         # 画像編集ライブラリテスト用★
         original = Image.open(__UPLOADS__ + "59b6c1c9-1fd4-4664-89a7-9905c88b6e39.jpg") # ハードドライブから画像をロード
         blurred = original.filter(ImageFilter.BLUR) # 画像をぼかす
@@ -62,8 +63,8 @@ application = tornado.web.Application([
         (r"/upload", Upload),
         (r"/mobilenurserest", MobileNurseRest),
         ], debug=True)
- 
- 
+
+
 if __name__ == "__main__":
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
