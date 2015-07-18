a5f3a3f18b3a6db42bb1690020b7baa7859141fd
Naoki Ueda <naoki.dx.xb@gmail.com>
2015-07-18 23:52:28 +0900

    Webサーバ開放

--- 2_run_serverapp-test.sh
+++ 2_run_serverapp-test.sh
@@ -2,6 +2,6 @@
 #
 # docker run serverapp-test
 #
-docker run --name serverapp-test-up -i -t -d -p 8080:8080 -p 8888:8888 \
+docker run --name serverapp-test-up -i -t -d -p 8080:8080 -p 80:80 -p 8888:8888 \
   -v $GIT_HOME/serverapp:/opt/t4j/serverapp \
   t4j/serverapp-test:latest
